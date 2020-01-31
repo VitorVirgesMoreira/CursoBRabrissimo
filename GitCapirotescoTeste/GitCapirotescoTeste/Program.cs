fb044be3e18e6cbcef6f44c964bec68cb0136d72
vitor virges <vitor.virges@gmail.com>
2020-01-31 16:10:35 -0300

    alteração na master

--- GitCapirotescoTeste/GitCapirotescoTeste/Program.cs
+++ GitCapirotescoTeste/GitCapirotescoTeste/Program.cs
@@ -10,7 +10,7 @@ namespace GitCapirotescoTeste
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("O curso é muito BRABO!!!!!!!!!!");
+            Console.WriteLine("O CURSO É MUITO BRABO!");
         }
     }
 }
