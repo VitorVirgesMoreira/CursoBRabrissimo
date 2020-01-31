c750b5ab060ebd04f92736bfc98104fc0b0a4ba0
vitor virges <vitor.virges@gmail.com>
2020-01-31 16:22:04 -0300

    é VDD ESSE BRANCH

--- GitCapirotescoTeste/GitCapirotescoTeste/Program.cs
+++ GitCapirotescoTeste/GitCapirotescoTeste/Program.cs
@@ -10,7 +10,7 @@ namespace GitCapirotescoTeste
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("O CURSO É MUITO BRABO!");
+            Console.WriteLine("O CURSO É UM POUCO BRABINHO!");
         }
     }
 }
